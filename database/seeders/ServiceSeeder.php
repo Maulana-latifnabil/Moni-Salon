10f7228212bed5e2bc4c65dd9f517a901ba5bc85
Maulana-latifnabil <latifnabilmaulana@gmail.com>
2025-04-25 23:21:36 +0700

    first commit

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -10,9 +10,9 @@ class ServiceSeeder extends Seeder
 {
     public function run()
     {
-        Service::create(['name' => 'Hair Color', 'price' => 150000]);
-        Service::create(['name' => 'Creambath', 'price' => 40000]);
-        Service::create(['name' => 'Haircut Dewasa', 'price' => 50000]);
-        Service::create(['name' => 'Haircut Anak-anak', 'price' => 35000]);
+        // Service::create(['name' => 'Hair Color', 'price' => 150000]);
+        // Service::create(['name' => 'Creambath', 'price' => 40000]);
+        // Service::create(['name' => 'Haircut Dewasa', 'price' => 50000]);
+        // Service::create(['name' => 'Haircut Anak-anak', 'price' => 35000]);
     }
 }
